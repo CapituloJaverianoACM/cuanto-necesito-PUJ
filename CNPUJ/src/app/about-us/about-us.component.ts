a6a041cc34b44a31649416c97ac2ebe4656c79fa
juan-hamon <53021906+juan-hamon@users.noreply.github.com>
2021-07-02 15:11:24 -0500

    Finalización de las rutas y correcciones a ciertos elementos de las pantallas

--- CNPUJ/src/app/about-us/about-us.component.ts
+++ CNPUJ/src/app/about-us/about-us.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
+  information = [
+    {
+      name: 'Juan Francisco Hamon',
+      description: 'Estudiante de ingeniería de sistemas de la Pontificia Universidad Javeriana con énfasis en construcción de software y sistemas inteligentes.',
+      image: 'assets/photos/Juan_Hamon.jpg'
+    },
+    {
+      name: 'Camilo Andrés Buitrago',
+      description: 'Estudiante de ingeniería de sistemas de la Pontificia Universidad Javeriana con énfasis en construcción de software.',
+      image: 'assets/photos/Camilo_Buitrago.jpg'
+    },
+    {
+      name: 'Kevin Andrés Garzón',
+      description: 'Estudiante de ingeniería de sistemas de la Pontificia Universidad Javeriana, 20 años, Interesado en la innovación y el aprender constante.',
+      image: 'assets/photos/Kevin_Garzon.jpg'
+    },
+    {
+      name: 'Camilo Alejandro Nossa',
+      description: 'Estudiante de ingeniería de sistemas de la Pontificia Universidad Javeriana, apasionado por la tecnología y la informática, dispuesto a aprender algo nuevo cada día.',
+      image: 'assets/photos/Camilo_Nossa.jpg'
+    },
+    {
+      name: 'Santiago Camilo Rey',
+      description: 'Estudiante de ingeniería de sistemas de la Pontificia Universidad Javeriana de 18 años. Soy una persona alegre, ingeniosa, y siempre dispuesto a aprender.',
+      image: 'assets/photos/Santiago_Rey.jpg'
+    }
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
--- CNPUJ/src/app/app-routing.module.ts
+++ CNPUJ/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AboutUsComponent } from "./about-us/about-us.component";
 import { CNBodyComponent } from "./cn-body/cn-body.component";
 import { MainBodyComponent } from "./main-body/main-body.component";
 
@@ -9,6 +10,7 @@ const routes: Routes = [
         { path: 'grades', component: MainBodyComponent },
         { path: 'academic', component: MainBodyComponent }
     ] },
+    { path: 'about-us', component: AboutUsComponent }
 ];
 
 @NgModule({
